0c2e3df0e30173248fcdc54543144fa4281b3bd5
Superfly <danielukoha101@gmail.com>
2022-07-29 07:01:05 +0100

    replacing login modal with a login page

--- Front-end/sign-up.js
+++ Front-end/sign-up.js
@@ -42,3 +42,61 @@ togglePassword.addEventListener("click", (e) => {
   // toggle the eye slash icon
   togglePassword.classList.toggle("hide");
 });
+
+const inputs = document.querySelectorAll(".reg-form .validate");
+
+const form = document.querySelector(".reg-form");
+// console.log(inputs);
+// console.log(form);
+
+const inputHandler = (event) => {
+  if (event.target.value.trim() === "") {
+    showError(event.target);
+  } else {
+    clearError(event.target);
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("blur", inputHandler);
+  input.addEventListener("input", inputHandler);
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(inputs);
+  console.log(form);
+  inputs.forEach((input) => {
+    if (input.type === "email") {
+      if (input.value === "" || !isValid(input.value)) {
+        showError(input);
+      }
+    } else {
+      if (input.value === "") {
+        showError(input);
+      }
+    }
+  });
+  const errorInput = document.querySelector(".has-error input:first-child");
+  if (errorInput) {
+    document.querySelector(".has-error input:first-child").focus();
+    return;
+  }
+  inputs.forEach((input) => (input.value = ""));
+});
+
+function showError(input) {
+  const parent = input.parentElement;
+  parent.className = "form-control validate has-error";
+}
+
+function clearError(input) {
+  const parent = input.parentElement;
+  parent.className = "form-control validate";
+}
+
+function isValid(email) {
+  return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}

112f5496b4aeb3662f78b15af05bf2a7913a2409
Jaay06 <josekoyi9@gmail.com>
2022-08-02 22:35:39 +0100

    mobile view

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ function navShadow() {
     navBar.classList.remove('floatingNav')
   }
 }
+
+//hambuger menu
+
+const menuBtn = document.querySelector('.hamburger')
+const mobileMenu = document.querySelector('.mobile-nav')
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('is-active')
+  mobileMenu.classList.toggle('is-active')
+})

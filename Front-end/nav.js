8892f95ac5e726cdc6ff6c761c6b62b9c0a2bf04
Superfly <danielukoha101@gmail.com>
2022-08-04 20:59:15 +0100

    updating landing page

--- Front-end/nav.js
+++ Front-end/nav.js
@@ -1,21 +1,21 @@
-const menuBtn = document.querySelector('.hamburger')
-const mobileMenu = document.querySelector('.mobile-nav')
+const menuBtn = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile-nav");
 
-menuBtn.addEventListener('click', () => {
-  menuBtn.classList.toggle('is-active')
-  mobileMenu.classList.toggle('is-active')
-})
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("is-active");
+  mobileMenu.classList.toggle("is-active");
+});
 
 // sticky navbar
-const navBar = document.querySelector('.navbar')
+// const navBar = document.querySelector('.navbar')
 
-document.addEventListener('scroll', navShadow)
+// document.addEventListener('scroll', navShadow)
 
-// navShadow effect
-function navShadow() {
-  if (window.scrollY > 145) {
-    navBar.classList.add('floatingNav')
-  } else {
-    navBar.classList.remove('floatingNav')
-  }
-}
+// // navShadow effect
+// function navShadow() {
+//   if (window.scrollY > 145) {
+//     navBar.classList.add('floatingNav')
+//   } else {
+//     navBar.classList.remove('floatingNav')
+//   }
+// }

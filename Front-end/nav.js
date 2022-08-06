b27a92d866de6ee287421d8023c3dca20fe11f99
Superfly <danielukoha101@gmail.com>
2022-08-06 10:29:06 +0100

    adding documentation, updating sign up

--- Front-end/nav.js
+++ Front-end/nav.js
@@ -1,21 +1,21 @@
-const menuBtn = document.querySelector('.hamburger')
-const mobileMenu = document.querySelector('.mobile-nav')
+const menuBtn = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile-nav");
 
-menuBtn.addEventListener('click', () => {
-  menuBtn.classList.toggle('is-active')
-  mobileMenu.classList.toggle('is-active')
-})
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("is-active");
+  mobileMenu.classList.toggle("is-active");
+});
 
 // sticky navbar
-const navBar = document.querySelector('.navbar')
+const navBar = document.querySelector("header");
 
-document.addEventListener('scroll', navShadow)
+document.addEventListener("scroll", navShadow);
 
 // navShadow effect
 function navShadow() {
   if (window.scrollY > 145) {
-    navBar.classList.add('floatingNav')
+    navBar.classList.add("floatingNav");
   } else {
-    navBar.classList.remove('floatingNav')
+    navBar.classList.remove("floatingNav");
   }
 }
